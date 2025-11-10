4f87558d72c7d9ec5542012a2016ca7dc6293a96
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-11-10 06:14:56 +0100

    update

--- Cours_FPS/Assets/[Project]/Scripts/Gameplay/PlayerLook.cs
+++ Cours_FPS/Assets/[Project]/Scripts/Gameplay/PlayerLook.cs
@@ -6,18 +6,19 @@ public class PlayerLook : MonoBehaviour
 {
     [SerializeField] private Transform _orientationContainer;
     [SerializeField] private float _lookSensitivity = 2f;
+    [SerializeField] private float _lookSensitivityAim = 2f;
     [SerializeField] private float _maxLookAngle = 90f;
     private Vector2 _lookInput;
     private float _xRotation = 0f;
+    private bool _isAiming = false;
 
 
-    private void FixedUpdate()
+    private void Update()
     {
-        float mouseX = _lookInput.x * _lookSensitivity * Time.fixedDeltaTime;
+        float mouseX = _lookInput.x * Time.deltaTime * (_isAiming ? _lookSensitivityAim : _lookSensitivity);
         transform.Rotate(0, mouseX, 0);
 
-
-        float mouseY = _lookInput.y * _lookSensitivity * Time.fixedDeltaTime;
+        float mouseY = _lookInput.y * _lookSensitivity * Time.deltaTime;
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -_maxLookAngle, _maxLookAngle);
         _orientationContainer.localEulerAngles = new Vector3(_xRotation, 0, 0);
